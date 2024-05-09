64d1cf527a4f2fb3d04d9b7c0e8ade55f87fff66
Marcus Rådell <marcus@radell.net>
2024-05-09 15:00:39 +0200

    pairs that are equal should collide

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,12 @@
 import test from "node:test";
 import assert from "node:assert";
 
-test("test", (t) => {
-  assert.strictEqual(1, 1);
+type Pair = [number, number];
+
+const checkCollision = (pairA: Pair, pairB: Pair) => {
+  return true;
+};
+
+test("pairs that are equal should collide", (t) => {
+  assert.strictEqual(checkCollision([0, 1], [0, 1]), true);
 });
