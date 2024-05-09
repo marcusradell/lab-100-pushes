8206a1c8e4c4d2d8dc55bc10f449ec5952dfba92
Marcus Rådell <marcus@radell.net>
2024-05-09 15:14:12 +0200

    change to a safer assertion

--- src/pair.test.ts
+++ src/pair.test.ts
@@ -3,5 +3,9 @@ import test from "node:test";
 import { createPair } from "./pair";
 
 test("two numbers should create a pair", () => {
-  assert.deepEqual(createPair(0, 1), [0, 1]);
+  assert.deepStrictEqual(createPair(0, 1), [0, 1]);
+});
+
+test("lowest number should be first", () => {
+  assert.deepStrictEqual(createPair(10, 5), [5, 10]);
 });
