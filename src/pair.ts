cf1af95f493fe1659fb38284bec52dcb35398701
Marcus Rådell <marcus@radell.net>
2024-05-09 15:17:01 +0200

    lowest number should be first

--- src/pair.ts
+++ src/pair.ts
@@ -1,3 +1,4 @@
 export type Pair = [number, number];
 
-export const createPair = (a: number, b: number) => [a, b];
+export const createPair = (a: number, b: number): Pair =>
+  a <= b ? [a, b] : [b, a];
