b36e764a26e507d4af7705850997702dba44d852
Marcus Rådell <marcus@radell.net>
2024-05-09 15:07:19 +0200

    two numbers should create a pair

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,10 +3,16 @@ import assert from "node:assert";
 
 type Pair = [number, number];
 
+const createPair = (a: number, b: number) => [a, b];
+
 const checkCollision = (pairA: Pair, pairB: Pair) => {
   return true;
 };
 
-test("pairs that are equal should collide", (t) => {
-  assert.strictEqual(checkCollision([0, 1], [0, 1]), true);
+test("pairs that are equal should collide", () => {
+  assert.deepEqual(checkCollision([0, 1], [0, 1]), true);
+});
+
+test("two numbers should create a pair", () => {
+  assert.deepEqual(createPair(0, 1), [0, 1]);
 });
