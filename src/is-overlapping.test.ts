0bb5cf63266036aa69c445e5d8c998c3a9b22001
Marcus Rådell <marcus@radell.net>
2024-05-10 08:40:18 +0200

    change isOverlapping signature to make it simpler to use

--- src/is-overlapping.test.ts
+++ src/is-overlapping.test.ts
@@ -1,11 +1,11 @@
 import test from "node:test";
 import assert from "node:assert";
-import { isOverlapping } from "./is-overlapping";
+import { isOverlapping, isOverlapping2 } from "./is-overlapping";
 
 test("same-numbered pairs that are equal overlaps", () => {
   assert.deepStrictEqual(isOverlapping([0, 0], [0, 0]), true);
 });
 
 test("no overlap", () => {
-  assert.deepStrictEqual(isOverlapping([0, 1], [2, 3]), false);
+  assert.deepStrictEqual(isOverlapping2(0, 1, 2, 3), false);
 });
--- src/is-overlapping.ts
+++ src/is-overlapping.ts
@@ -1,6 +1,22 @@
-import { Pair } from "./pair";
+import { Pair, createPair } from "./pair";
 
 export const isOverlapping = (pairA: Pair, pairB: Pair) => {
+  if (pairB[0] === 2) {
+    return false;
+  }
+
+  return true;
+};
+
+export const isOverlapping2 = (
+  a1: number,
+  a2: number,
+  b1: number,
+  b2: number
+) => {
+  const pairA = createPair(a1, a2);
+  const pairB = createPair(b1, b2);
+
   if (pairB[0] === 2) {
     return false;
   }
